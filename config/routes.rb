dbf36454518c14c39aea4bc957c1f146c71f9499
komaf5436 <k.3624.t.aw@gmail.com>
2022-04-18 16:51:30 +0000

    Add usersコントローラー作成　ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,29 @@
 Rails.application.routes.draw do
 
+  # namespace :public do
+    # get 'users/index'
+    # get 'users/show'
+    # get 'users/edit'
+  # end
+  
   # ユーザー用
   # URL/users/sign_in...
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => "homes#about", as: 'about'
+    resources :users, only: [:index, :show, :edit]
+  end
 
   # 管理者用
   # URL/admin/sign_in...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
-  root to: "public/homes#top"
-  get 'public/homes/about' => "public/homes#about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
