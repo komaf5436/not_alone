0eb8a9921960262178556ff780a57125b9a4dcc1
komaf5436 <k.3624.t.aw@gmail.com>
2022-04-19 14:59:30 +0000

    Add public/user編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,21 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :posts, dependent: :destroy
+  
+  has_one_attached :profile_image
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  
+  def get_profile_image
+    (profile_image.attached) ? profile_image : 'no_image.jpg'
+  end
+  
 end
