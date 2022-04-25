f2f079460af244c2898e2234d11241a5c93287cd
komaf5436 <k.3624.t.aw@gmail.com>
2022-04-25 18:31:06 +0000

    Add adminのpostsコントローラー作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -29,6 +29,7 @@ class Admin::SessionsController < Devise::SessionsController
   
   # 投稿一覧
   def after_sign_in_path_for(resource)
+    admin_root_path
   end
   
   def after_sign_out_path_for(resource)
